c8ef9a9f982abc89ffa5432a5025135843ac5fed
Artem <mr.ponomarenko.a@mail.ru>
2022-12-30 00:51:32 +0300

    Создано окно, класс Player

---
+++ main.py
@@ -0,0 +1,99 @@
+import os
+import sys
+import pygame
+from pygame.locals import * #это добавляет обработку клавиш
+
+pygame.init()
+
+all_sprites = pygame.sprite.Group()
+
+player_group = pygame.sprite.Group()
+
+FPS = 50
+
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+clock = pygame.time.Clock()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = load_image("player.png")
+        self.rect = self.image.get_rect().move(pos_x, pos_y)
+
+
+
+    def move(self, movement):
+        x, y = self.rect
+
+        if movement == 'up':
+            y -= 10
+        elif movement == 'down':
+            y += 10
+        elif movement == 'left':
+            x -= 10
+        elif movement == 'right':
+            x += 10
+
+
+        self.rect = self.image.get_rect().move(x,y)
+
+
+def start():
+
+
+
+    map_house = pygame.transform.scale(load_image('карта_дом.png'), (
+        pygame.display.get_surface().get_size()))  # Загрузать и изменить карту под размер окна
+
+    screen.blit(map_house, (0, 0))
+
+    running = True
+    player = Player(500, 500)
+
+    while running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+
+        if pygame.key.get_pressed()[K_RIGHT] or pygame.key.get_pressed()[K_d]:
+            player.rect.left += 10
+        if pygame.key.get_pressed()[K_UP] or pygame.key.get_pressed()[K_w]:
+            player.rect.top -= 10
+        if pygame.key.get_pressed()[K_DOWN] or pygame.key.get_pressed()[K_s]:
+            player.rect.top += 10
+        if pygame.key.get_pressed()[K_LEFT] or pygame.key.get_pressed()[K_a]:
+            player.rect.left -= 10
+
+
+
+
+
+
+        screen.blit(map_house, (0, 0))
+        player_group.draw(screen)
+
+        pygame.display.flip()
+
+        clock.tick(FPS)
+
+
+# -------------------------------------------------------------------------------------------------------------------
+
+
+start()
+
+pygame.quit()
+sys.exit()
